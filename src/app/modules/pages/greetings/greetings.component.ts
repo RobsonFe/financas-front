e48df41a8b3f9f8c877a13f5ed0b295c4b89e571
RobsonFe <robson_20ferreira@hotmail.com>
2024-08-24 21:39:34 -0300

    update:greeting

--- src/app/modules/pages/greetings/greetings.component.ts
+++ src/app/modules/pages/greetings/greetings.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FlowbiteService } from '../../service/flowbite.service';
 
 @Component({
   selector: 'app-greetings',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './greetings.component.html',
-  styleUrl: './greetings.component.css'
+  styleUrl: './greetings.component.css',
 })
-export class GreetingsComponent {
-
+export class GreetingsComponent implements OnInit {
+  constructor(private flowbiteService: FlowbiteService) {}
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
 }
